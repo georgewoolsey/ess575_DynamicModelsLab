0cde91451457f14d7fadac533c67d2de5893ad49
georgewoolsey <george.woolsey@colostate.edu>
2022-12-01 15:07:02 -0700

    updating

--- src/LynxJAGS.R
+++ src/LynxJAGS.R
@@ -10,7 +10,7 @@
       tau <- 1/sigma.p^2
       # initial conditions informed priors
       N[1] ~ dlnorm(log(y[1]/phi) , tau)
-      mu[1] <- N[1] * phi # mean family group size
+      fam_grps[1] <- N[1] * phi # mean family groups (mu)
       
     ###########################
     # likelihood
@@ -19,13 +19,14 @@
       for(t in 2:(y.endyr+1)){
     	  alpha[t] <- lambda * (N[t-1] - y.H[t-1])
     	  N[t] ~ dlnorm(log(max(alpha[t], 0.000001)), tau) # can't take the log of alpha <= 0
-    	  # ????
+    	  # calculate the mean for use in the data model
+    	    # include in this loop to get the forecast value for #6
+    	  fam_grps[t] <- N[t] * phi # mean family groups (mu)
     	}
       # Data model:
       for(t in 2:y.endyr){
-        mu[t] <- N[t] * phi # mean family group size
         # returns density (for continuous) because l.h.s. is data (deterministic b/c defined in data)
-    	    y[t] ~ dpois(mu[t]) 
+    	    y[t] ~ dpois(fam_grps[t]) 
       }
     
     ###########################
@@ -35,16 +36,16 @@
         # have to put this in own loop because y[1] defined separately 
         for(t in 1:y.endyr){
           # returns random number generator because l.h.s. is not data (i.e. it is unknown: stochastic node)
-            y_sim[t] ~ dpois(mu[t]) 
+            y_sim[t] ~ dpois(fam_grps[t]) 
           # sum of squares 
-            sq[t] <- (y[t]-mu[t])^2
-            sq_sim[t] <- (y_sim[t]-mu[t])^2
+            sq[t] <- (y[t]-fam_grps[t])^2
+            sq_sim[t] <- (y_sim[t]-fam_grps[t])^2
           # autocorrelation
             # Assure yourself that the process model adequately accounts for...
               # ...temporal autocorrelation in the residuals — allowing the assumption... 
               # ...that they are independent and identically distributed. 
               # To do this, include a derived quantity:
-            e[t] <- (y[t]-mu[t])
+            e[t] <- (y[t]-fam_grps[t])
         }
       #posterior predictive checks
         # test statistics y
